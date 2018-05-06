348f2b7b3bf49c3629c5064422ee1ab09d01f48f
Hitkul <hitkuljangid@gmail.com>
2018-05-06 12:20:25 +0530

    print statment fix

--- combine.py
+++ combine.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 filenames = ['Output_2017_08.txt','Output_2017_09.txt','Output.txt']
 with open('data/raw_twitter_data.txt', 'w') as outfile:
     for fname in filenames:
-        print("writing from  ",filenames)
+        print("writing from  ",fname)
         with open('data/'+fname) as infile:
             for line in infile:
                 outfile.write(line)
