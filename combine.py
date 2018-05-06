4f8c62e836c38aebdf10300397810b5851635cb0
Hitkul <hitkuljangid@gmail.com>
2018-05-06 12:17:22 +0530

    script to combine multiple files into one

--- combine.py
+++ combine.py
@@ -1,6 +1,9 @@
-filenames = []
-with open('raw_twitter_data.txt', 'w') as outfile:
+from __future__ import print_function
+
+filenames = ['Output_2017_08.txt','Output_2017_09.txt','Output.txt']
+with open('data/raw_twitter_data.txt', 'w') as outfile:
     for fname in filenames:
-        with open(fname) as infile:
+        print("writing from  ",filenames)
+        with open('data/'+fname) as infile:
             for line in infile:
                 outfile.write(line)
