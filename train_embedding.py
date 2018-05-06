480eb88ed676c34185b5ee9395c012ef5ce58b24
Hitkul <hitkuljangid@gmail.com>
2018-05-06 16:31:36 +0530

    counting done

--- train_embedding.py
+++ train_embedding.py
@@ -1,6 +1,14 @@
+from collections import Counter
+
 input_file_path = 'temp.txt'
 output_path = 'twitter_fasttext'
 
 with open(input_file_path,'r') as fin:
     lines = fin.readlines()
 
+counts = Counter()
+
+for line in lines:
+    counts.update(word for word in line.split())
+
+print(counts)
