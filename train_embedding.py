77ac210c73d3fc2a70e369f78fb8a3197b003314
Hitkul <hitkuljangid@gmail.com>
2018-05-06 16:34:51 +0530

    writing to json

--- train_embedding.py
+++ train_embedding.py
@@ -2,7 +2,8 @@ from collections import Counter
 import json
 
 input_file_path = 'temp.txt'
-output_path = 'twitter_fasttext'
+output_path = 'twitter_fasttext_clean_level2'
+output_path_json = "tf_clean_level2.json"
 
 with open(input_file_path,'r') as fin:
     lines = fin.readlines()
@@ -12,5 +13,5 @@ counts = Counter()
 for line in lines:
     counts.update(word for word in line.split())
 
-with open('test.json','w') as fout:
+with open(output_path_json,'w') as fout:
     json.dump(counts,fout,indent=4)
