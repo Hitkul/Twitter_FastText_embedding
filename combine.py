cc1c8242177be3a9d637a2e99174aabeed064d11
Hitkul <hitkuljangid@gmail.com>
2018-05-06 12:21:43 +0530

    more print

--- combine.py
+++ combine.py
@@ -4,6 +4,9 @@ filenames = ['Output_2017_08.txt','Output_2017_09.txt','Output.txt']
 with open('data/raw_twitter_data.txt', 'w') as outfile:
     for fname in filenames:
         print("writing from  ",fname)
+        count = 0
         with open('data/'+fname) as infile:
             for line in infile:
+                print("writing line number ", count,end='\r')
                 outfile.write(line)
+                count+=1
