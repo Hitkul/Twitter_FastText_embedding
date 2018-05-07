d4e29002e2e6a0dfb107e14b1c316f908b01e05e
Hitkul <hitkuljangid@gmail.com>
2018-05-07 10:28:20 +0530

    total count added

--- train_embedding.py
+++ train_embedding.py
@@ -10,18 +10,26 @@ print("reading file")
 with open(input_file_path,'r') as fin:
     lines = fin.readlines()
 
+number_of_tweets = len(lines)
+
+meta_data = dict()
+
+meta_data['total_number_of_tweets'] = number_of_tweets
+
 counts = Counter()
 
 print("counting terms")
 for line in lines:
     counts.update(word for word in line.split())
 
+meta_data['tf'] = counts
+
 print("writing terms to json")
 with open(output_path_json,'w') as fout:
-    json.dump(counts,fout,indent=4)
+    json.dump(meta_data,fout,indent=4)
 
 dimensions = [50,100,300]
 
 for d in dimensions:
-    print('print traning of dimension ',d)
+    print('traning of dimension ',d)
     model = fasttext.cbow(input_file_path, output_path+"_"+str(d), dim=d)
